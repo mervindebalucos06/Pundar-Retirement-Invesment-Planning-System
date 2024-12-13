933868eb821377c1a04b895847c5eb4b715cbfe7
Mervin <mervidebalucos@gmail.com>
2024-12-14 04:45:33 +0800

    style: enhance main menu display for better user experience

--- Main.java
+++ Main.java
@@ -8,11 +8,19 @@ public class Main {
 
     try (Scanner input = new Scanner(System.in)) { // Use try-with-resources for Scanner
       while (!exit) {
-        System.out.println("\n=== Pundar: Retirement Investment Planning System ===");
-        System.out.println("1. Sign Up");
-        System.out.println("2. Login");
-        System.out.println("3. About Us");
-        System.out.println("4. Exit");
+        System.out.println();
+        System.out.println("===============================================");
+        System.out.println("        P---  U---  N---  D---  A---  R---    ");
+        System.out.println("===============================================");
+        System.out.println(" *** Retirement Investment Planning System ***");
+        System.out.println("===============================================");
+        System.out.println("        Plan today for a secure tomorrow!      ");
+        System.out.println("===============================================");
+        System.out.println();
+        System.out.println("\t\t1. Sign Up");
+        System.out.println("\t\t2. Login");
+        System.out.println("\t\t3. About Us");
+        System.out.println("\t\t4. Exit");
         System.out.print("Choose an option: ");
 
         int choice = input.nextInt();
